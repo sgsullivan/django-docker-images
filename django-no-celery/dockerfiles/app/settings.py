859928d869b20155358be7136c5e26c2ee8c0be2
Scott Sullivan <scottgregorysullivan@gmail.com>
2020-03-17 20:35:46 -0400

    move to an image

--- django-no-celery/dockerfiles/app/settings.py
+++ django-no-celery/dockerfiles/app/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'T04st3rZ',
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
         'HOST': 'db',
         'PORT': 5432,
     }
