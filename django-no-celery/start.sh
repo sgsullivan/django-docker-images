2338f4f1accfdf7865ee0e5fe65788cacaab7907
Scott Sullivan <scottgregorysullivan@gmail.com>
2020-03-17 20:55:32 -0400

    web port must not be hard coded

--- django-no-celery/start.sh
+++ django-no-celery/start.sh
@@ -3,5 +3,6 @@
 set -eu
 
 PROJECT_NAME=$1
+WEB_PORT=$2
 
-export COMPOSE_PROJECT_NAME=${PROJECT_NAME} && export POSTGRES_PASSWORD=${POSTGRES_PASSWORD} && docker-compose -p ${PROJECT_NAME} up --build -d
+export WEB_PORT=${WEB_PORT} && export COMPOSE_PROJECT_NAME=${PROJECT_NAME} && export POSTGRES_PASSWORD=${POSTGRES_PASSWORD} && docker-compose -p ${PROJECT_NAME} up --build -d
