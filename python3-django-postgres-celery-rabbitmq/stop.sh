64681d0dcd0326d2902f2352e472b72339d8608a
Scott Sullivan <scottgregorysullivan@gmail.com>
2020-03-18 19:38:47 -0400

    stop needs ssh port

--- python3-django-postgres-celery-rabbitmq/stop.sh
+++ python3-django-postgres-celery-rabbitmq/stop.sh
@@ -4,5 +4,10 @@ set -eu
 
 PROJECT_NAME=$1
 WEB_PORT=$2
+SSH_PORT=$3
 
-export WEB_PORT=${WEB_PORT} && export COMPOSE_PROJECT_NAME=${PROJECT_NAME} && docker-compose -p ${PROJECT_NAME} down
+export WEB_PORT=${WEB_PORT}
+export SSH_PORT=${SSH_PORT}
+export COMPOSE_PROJECT_NAME=${PROJECT_NAME}
+
+docker-compose -p ${PROJECT_NAME} down
