9261bbba22af7e4c36f0deaa8a064404d3c82a3f
Scott Sullivan <scottgregorysullivan@gmail.com>
2020-03-17 20:59:31 -0400

    include WEB_PORT in stop

--- django-no-celery/stop.sh
+++ django-no-celery/stop.sh
@@ -3,5 +3,6 @@
 set -eu
 
 PROJECT_NAME=$1
+WEB_PORT=$2
 
-export COMPOSE_PROJECT_NAME=${PROJECT_NAME} && docker-compose -p ${PROJECT_NAME} down
+export WEB_PORT=${WEB_PORT} && export COMPOSE_PROJECT_NAME=${PROJECT_NAME} && docker-compose -p ${PROJECT_NAME} down
