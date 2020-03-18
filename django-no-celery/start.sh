2e66a397b2d906cf30b1c256d4254613fffe2277
Scott Sullivan <scottgregorysullivan@gmail.com>
2020-03-17 20:24:56 -0400

    adjust

--- django-no-celery/start.sh
+++ django-no-celery/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export COMPOSE_PROJECT_NAME=erste && export POSTGRES_PASSWORD=T04st3rZ && docker-compose -p erste up --build -d
+export COMPOSE_PROJECT_NAME=erste && export POSTGRES_PASSWORD=p455w0rd && docker-compose -p erste up --build -d
 
--- django-no-celery/stop.sh
+++ django-no-celery/stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose -p erste down
+export COMPOSE_PROJECT_NAME=erste && export POSTGRES_PASSWORD=p455w0rd && docker-compose -p erste down
