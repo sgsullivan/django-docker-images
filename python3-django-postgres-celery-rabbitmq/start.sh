895dc0a5dfb9f4cabb366fabc331bbd619fbb1b4
Scott Sullivan <scottgregorysullivan@gmail.com>
2020-03-18 22:55:09 -0400

    add vars to profile

--- python3-django-postgres-celery-rabbitmq/start.sh
+++ python3-django-postgres-celery-rabbitmq/start.sh
@@ -24,6 +24,17 @@ useradd ${PROJECT_NAME} -d /home/${PROJECT_NAME} -s /bin/bash
   echo "${SSH_PUB_KEY}" > /var/sites/${PROJECT_NAME}/django/.ssh/authorized_keys
 }
 
+cat << EOF > /var/sites/${PROJECT_NAME}/django/.profile
+export POSTGRES_PASSWORD=${POSTGRES_PASSWORD}
+export COMPOSE_PROJECT_NAME=${PROJECT_NAME}
+export RMQ_CLUSTER=${RMQ_CLUSTER}
+export RMQ_USER=${PROJECT_NAME}
+export RMQ_PASSWORD=${RMQ_PASSWORD}
+
+alias ls="ls --color"
+alias ll="ls -la --color"
+EOF
+
 chown -R ${PROJECT_NAME}.${PROJECT_NAME} /var/sites/${PROJECT_NAME}/django
 
 docker-compose -p ${PROJECT_NAME} up --build -d
